ada4a1ebcb3930ee1d285a004d5d4654411ddae1
chinamiii <chinami.2929@icloud.com>
2021-09-08 15:39:13 +0000

    [start]

---
+++ app/models/book.rb
@@ -0,0 +1,45 @@
+class Book < ApplicationRecord
+
+    belongs_to :user
+    has_many :book_comments, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+
+    has_many :favorited_users, through: :favorites, source: :user
+
+    is_impressionable counter_cache: true
+
+
+    validates :title, presence: true
+
+    validates :body, presence: true,
+                     length: { maximum: 200 }
+
+
+    def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
+
+    def self.search(search, word)
+        if search == "forward_match"
+                        @book = Book.where("title LIKE?","#{word}%")
+        elsif search == "backward_match"
+                        @book = Book.where("title LIKE?","%#{word}")
+        elsif search == "perfect_match"
+                        @book = Book.where("title LIKE?","#{word}")
+        elsif search == "partial_match"
+                        @book = Book.where("title LIKE?","%#{word}%")
+        else
+                        @book = Book.all
+        end
+    end
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
+
+
+
+
+
+end
